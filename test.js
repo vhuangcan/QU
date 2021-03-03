5fa3e4f60775b3aafbe17f57a702293183717e3c
huangcan <51341644@qq.com>
2021-03-03 16:32:04 +0800

    success

--- test.js
+++ test.js
@@ -15,7 +15,7 @@
     'Accept': `application/json, text/javascript, */*; q=0.01`,
     'Accept-Encoding': `gzip, deflate, br`,
     'Origin': `https://www.v2e.fun`,
-    'Cookie': `email=304591874%40qq.com; expire_in=1617347672; ip=8351ace6e0cb060a3ff9811ecfa95e7a; key=5e48b96fcb47c0992c2140b5167cb38323825a98d2697; uid=53151; __cfduid=deb2a93203aa2f2b2ed5ac9c80907c50f1614755641`,
+    'Cookie': `uid=5329; email=513416443%40qq.com; key=9f21897805252c987bab48f1abd1c58253b62027a568e; ip=90df72bd0cf7fdf4e983a9efbee301fe; expire_in=1617351631`,
     'Connection': `keep-alive`,
     'Host': `www.v2e.fun`,
     'User-Agent': `Mozilla/5.0 (iPhone; CPU iPhone OS 14_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Mobile/15E148 Safari/604.1`,
@@ -37,9 +37,7 @@
         console.log(`签到执行结束！`)
         $done();
       }, response => {
-        console.log('',
-            `❌ ${name}, 失败! 原因: ${response.error}!`
-            , '')
+        console.log( `❌ ${name}, 失败! 原因: ${response.error}!`)
         $notify(name, '失败！')
         $done();
       });
