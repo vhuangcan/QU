8092b49a37e258380d70cb1bc3856504e2c449fa
huangcan <51341644@qq.com>
2021-03-03 15:58:38 +0800

    success

--- test.js
+++ test.js
@@ -5,9 +5,8 @@
  * Time: 2:52 下午
  * tips:
  */
-const $ = new Env('豆奶签到')
 
-$.log('', `🔔 ${$.name}, 开始!`, '')
+const name = '豆奶签到'
 const url = `https://www.v2e.fun/user/checkin`;
 const method = `POST`;
 const headers = {
@@ -31,34 +30,15 @@ const myRequest = {
 };
 
 $task.fetch(myRequest).then(response => {
-  $.log(`${JSON.parse(response.body)}`)
-  $.msg($.name, '成功！')
+  console.log(`${JSON.parse(response.body)}`)
+  $notify(name, '成功！')
   $done();
 }, response => {
-  $.log('', `❌ ${$.name}, 失败! 原因: ${response.error}!`, '')
-  $.msg($.name, '失败')
+  console.log('', `❌ ${name}, 失败! 原因: ${response.error}!`, '')
+  $notify(name, '失败')
   $done();
 });
 
-// prettier-ignore
-function Env(t) {
-  this.name = t, this.logs = [], this.isSurge = (() => "undefined" != typeof $httpClient), this.isQuanX = (() => "undefined" != typeof $task), this.log = ((...t) => {
-    this.logs = [...this.logs, ...t], t ? console.log(t.join("\n")) : console.log(this.logs.join("\n"))
-  }), this.msg = ((t = this.name, s = "", i = "") => {
-    this.isSurge() && $notification.post(t, s, i), this.isQuanX() && $notify(t, s, i);
-    const e = ["", "==============\ud83d\udce3\u7cfb\u7edf\u901a\u77e5\ud83d\udce3=============="];
-    t && e.push(t), s && e.push(s), i && e.push(i), console.log(e.join("\n"))
-  }), this.getdata = (t => this.isSurge() ? $persistentStore.read(t) : this.isQuanX() ? $prefs.valueForKey(t) : void 0), this.setdata = ((t, s) => this.isSurge() ? $persistentStore.write(t, s) : this.isQuanX() ? $prefs.setValueForKey(t, s) : void 0), this.get = ((t, s) => this.send(t, "GET", s)), this.wait = ((t, s = t) => i => setTimeout(() => i(), Math.floor(Math.random() * (s - t + 1) + t))), this.post = ((t, s) => this.send(t, "POST", s)), this.send = ((t, s, i) => {
-    if (this.isSurge()) {
-      const e = "POST" == s ? $httpClient.post : $httpClient.get;
-      e(t, (t, s, e) => {
-        s && (s.body = e, s.statusCode = s.status), i(t, s, e)
-      })
-    }
-    this.isQuanX() && (t.method = s, $task.fetch(t).then(t => {
-      t.status = t.statusCode, i(null, t, t.body)
-    }, t => i(t.error, t, t)))
-  }), this.done = ((t = {}) => $done(t))
-}
+
 
 
