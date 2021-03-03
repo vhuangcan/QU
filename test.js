11b3a9fdb8a25a941ad57e91499b356375c5efbf
huangcan <51341644@qq.com>
2021-03-03 17:04:00 +0800

    success

--- test.js
+++ test.js
@@ -7,7 +7,9 @@
  */
 
 !(() => {
+  $prefs
   const name = '豆奶签到'
+  $prefs.valueForKey()
   console.log(`${name}开始执行！`)
   const url = `https://www.v2e.fun/user/checkin`;
   const method = `POST`;
@@ -15,7 +17,7 @@
     'Accept': `application/json, text/javascript, */*; q=0.01`,
     'Accept-Encoding': `gzip, deflate, br`,
     'Origin': `https://www.v2e.fun`,
-    'Cookie': `uid=5329; email=513416443%40qq.com; key=9f21897805252c987bab48f1abd1c58253b62027a568e; ip=90df72bd0cf7fdf4e983a9efbee301fe; expire_in=1617351631`,
+    'Cookie': `uid=5329; email=513416443%40qq.com; key=9f21897805252c987bab48f1abd1c58253b62027a568e; ip=90df72bd0cf7fdf4e983a9efbee301fe; expire_in=1737351631`,
     'Connection': `keep-alive`,
     'Host': `www.v2e.fun`,
     'User-Agent': `Mozilla/5.0 (iPhone; CPU iPhone OS 14_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Mobile/15E148 Safari/604.1`,
