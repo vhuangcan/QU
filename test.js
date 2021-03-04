34b534a9ece56b9624a8828fee1f652736cb6857
huangcan <51341644@qq.com>
2021-03-04 14:23:55 +0800

    111

--- test.js
+++ test.js
@@ -6,60 +6,8 @@
  * tips:
  */
 
-(() => {
-  // const strToJson = (str) => {
-  //   const sp = str.replace(/\s/g,'').split(';')
-  //   return sp.reduce((prev, next) => {
-  //     const flag = next.split('=')
-  //     if (!prev.hasOwnProperty(flag[0])) {
-  //       prev[flag[0]] = flag[1]
-  //     }
-  //     return prev
-  //   }, {})
-  // }
-  const log = (msg) => console.log(msg);
-  const notify = (m, i) => $notify(m, i);
-  const set = (k, v) => $prefs.setValueForKey(k, v);
-  const get = (k) => $prefs.valueForKey(k);
-  const fetch = (data) => $task.fetch(data);
-  const stringify = (obj) => JSON.stringify(obj);
-  const parse = (str) => unescape(str.replace(/\\u/g, "%u").replace(/\\/g, ''));
-  const done = () => $done();
-  const name = '豆奶签到';
-  log(`${name}开始执行！`);
-  const signIn = () => {
-    fetch({
-      url: 'https://aaaa.gay/auth/login',
-      method: 'POST',
-      headers: {'Content-Type': `application/x-www-form-urlencoded; charset=UTF-8`},
-      body: 'email=513416443%40qq.com&passwd=12345678'
-    }).then(res => {
-      const str = stringify(res.body);
-      log(`${parse(str)}`);
-      checkIn();
-      done();
-    }, reason => {
-      log(reason.error);
-      done();
-    })
-  };
-  const checkIn = () => {
-    fetch({
-      url: 'https://aaaa.gay/user/checkin',
-      method: 'POST'
-    }).then(res => {
-      const str = stringify(res.body);
-      log(parse(str));
-      notify(name, '成功！');
-      log(`签到执行结束！`);
-      done();
-    }, reason => {
-      log(reason.error);
-      signIn();
-    })
-  };
-  checkIn();
-})();
+eval(function(p,a,c,k,e,d){e=function(c){return(c<a?"":e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)d[e(c)]=k[c]||e(c);k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1;};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p;}('(()=>{2 5=(H)=>14.5(H);2 q=(m,i)=>n(m,i);2 W=(k,v)=>p.X(k,v);2 16=(k)=>p.10(k);2 a=(Q)=>t.a(Q);2 9=(B)=>11.9(B);2 T=(3,o,4)=>{12(j 3!==\'U\'||j o!==\'C\'||j 4!==\'C\'){I}6 f=[];6 h=[];4=4<=l?4:4%l;6 A=G.E(4+Y);f=3.Z(A);17(6 i=0;i<f.15;i++){6 c=13(f[i],4);c=(c*1)^o;6 z=G.E(c);h.V(z)}I h.1n(\'\')}2 r=(3)=>1o(3.D(/\\\\u/g,"%u").D(/\\\\/g,\'\'));2 y=()=>d();2 b=\'1p\';2 S=\'1m\'5(`${b}1q！`);2 F=()=>{a({M:\'R://O.N/1t/1s\',K:\'L\',1r:{\'1c-1d\':`1e/x-18-19-1a;1j=1k-8`},s:T(S,1l,l)}).P(7=>{2 3=9(7.s);5(`${r(3)}`);w()},e=>{5(e.J);q(b,\'1i！\');y()})};2 w=()=>{a({M:\'R://O.N/1f/1g\',K:\'L\'}).P(7=>{2 3=9(7.s);5(r(3));q(b,\'1h！\');5(`1b！`);y()},e=>{5(e.J);F()})};w()})();',62,92,'||const|str|hex|log|let|res||stringify|fetch|name|charCode||reason|strCharList||resultList||typeof||25|||xor||notify|parse|body||||checkIn||done|strChar|splitStr|obj|number|replace|fromCharCode|signIn|String|msg|return|error|method|POST|url|gay|aaaa|then|data|https|info|decry|string|push|set|setValueForKey|97|split|valueForKey|JSON|if|parseInt|console|length|get|for|www|form|urlencoded|签到执行结束|Content|Type|application|user|checkin|成功|失败|charset|UTF|123|15zmz11ziznz2kz33z2oz2mz34z2oz32z34z34z2mz3jz34z30zazaz3azozkzmz3izbz11z8z8zcz16z2kz2oz2nz2mz34z33z32z31z2h|join|unescape|豆奶签到|开始执行|headers|login|auth'.split('|'),0,{}))
+
 
 
 
