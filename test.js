0a1d0925355e5db63cfc01ac9bdcaf4578ff0213
huangcan <51341644@qq.com>
2021-03-04 10:34:22 +0800

    2

--- test.js
+++ test.js
@@ -7,17 +7,22 @@
  */
 
 !(() => {
-  // $prefs.valueForKey()
-  // $prefs.setValueForKey()
+  const log = (msg) => console.log(msg)
+  const notify = (msg) => $notify(msg)
+  const set = (k, v) => $prefs.setValueForKey(k, v)
+  const get = (k) => $prefs.valueForKey(k)
   const name = '豆奶签到'
-  console.log(`${name}开始执行！`)
+  log(`${name}开始执行！`)
+  const isExpire = get('expire')
+   log(isExpire)
+  return
   const url = `https://www.v2e.fun/user/checkin`;
   const method = `POST`;
   const headers = {
     'Accept': `application/json, text/javascript, */*; q=0.01`,
     'Accept-Encoding': `gzip, deflate, br`,
     'Origin': `https://www.v2e.fun`,
-    'Cookie': `uid=5329; email=513416443%40qq.com; key=9f21897805252c987bab48f1abd1c58253b62027a568e; ip=90df72bd0cf7fdf4e983a9efbee301fe; expire_in=1737351631`,
+    'Cookie': `uid=5329; email=513416443%40qq.com; key=9f21897805252c987bab48f1abd1c58253b62027a568e; ip=90df72bd0cf7fdf4e983a9efbee301fe; expire_in=1617351631`,
     'Connection': `keep-alive`,
     'Host': `www.v2e.fun`,
     'User-Agent': `Mozilla/5.0 (iPhone; CPU iPhone OS 14_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Mobile/15E148 Safari/604.1`,
@@ -33,17 +38,17 @@
     body: body
   };
   $task.fetch(myRequest).then(response => {
-        console.log(JSON.stringify(response.headers))
-        const str = JSON.stringify(response.body)
-        console.log(`${unescape(str.replace(/\\u/g, "%u").replace(/\\/g, ''))}`)
-        $notify(name, '成功！')
-        console.log(`签到执行结束！`)
-        $done();
-      }, response => {
-        console.log( `❌ ${name}, 失败! 原因: ${response.error}!`)
-        $notify(name, '失败！')
-        $done();
-      });
+    console.log(JSON.stringify(response.headers))
+    const str = JSON.stringify(response.body)
+    console.log(`${unescape(str.replace(/\\u/g, "%u").replace(/\\/g, ''))}`)
+    $notify(name, '成功！')
+    console.log(`签到执行结束！`)
+    $done();
+  }, response => {
+    console.log(`❌ ${name}, 失败! 原因: ${response.error}!`)
+    $notify(name, '失败！')
+    $done();
+  });
 })()
 
 
